a9db599b2dc162da4c3414e394cbb5c47341e128
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-02-21 18:53:42 -0300

    CategoryController

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using BlogEFCore.Data;
+using BlogEFCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers; 
+
+public class CategoryController : ControllerBase {
+
+    [HttpGet("categories")]
+    public IActionResult SelectAll([FromServices] DataContext context) {
+        List<Category> categories = [.. context.Categories];
+        return Ok(categories);
+    }
+}
