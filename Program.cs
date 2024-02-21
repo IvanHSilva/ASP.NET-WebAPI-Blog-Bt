72547d06e2389ffc9cf8ea9e8c84bfcf4f00d4f2
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-02-21 17:00:02 -0300

    HomeController

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using BlogEFCore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();
