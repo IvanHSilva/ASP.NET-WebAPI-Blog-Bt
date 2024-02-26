3438a55eb601a92bf771cfea4d052ebde049f06b
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-02-26 17:15:45 -0300

    CategoryViewModel validations

--- ViewModels/CategoryViewModel.cs
+++ ViewModels/CategoryViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace Blog.ViewModels; 
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels; 
 
 public class CategoryViewModel {
+    [Required(ErrorMessage = "Nome obrigatório")]
+    [MinLength(5, ErrorMessage = "Nome deve ter no mínimo 5 caracteres")]
     public string Name { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Slug obrigatório")]
+    [MinLength(5, ErrorMessage = "Slug deve ter no mínimo 5 caracteres")]
     public string Slug { get; set; } = string.Empty;
 }
