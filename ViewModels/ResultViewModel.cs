411bdcb8011503c9a500ca0925422921478a45b1
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-02-26 18:18:35 -0300

    ResultViewModel

---
+++ ViewModels/ResultViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels; 
+
+public class ResultViewModel<T> {
+ 
+    public T Data { get; private set; } = default!;
+    public List<string> Errors { get; private set; } = [];
+
+    public ResultViewModel(T data, List<string> errors) {
+        Data = data;
+        Errors = errors;
+    }
+    
+    public ResultViewModel(T data) => Data = data;
+
+    public ResultViewModel(List<string> errors) => Errors = errors;
+
+    public ResultViewModel(string error) => Errors.Add(error);
+}
+
