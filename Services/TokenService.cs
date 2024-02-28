135b429010bc324fd2cffa0568c7d6a7e21f4a5c
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-02-28 15:32:46 -0300

    TokenService

---
+++ Services/TokenService.cs
@@ -0,0 +1,18 @@
+﻿using BlogEFCore.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Blog.Services; 
+
+public class TokenService {
+
+    public string GenerateToken(User user) {
+
+        JwtSecurityTokenHandler tokenHandler = new();
+        byte[] key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        SecurityTokenDescriptor tokenDescriptor = new();
+        SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
